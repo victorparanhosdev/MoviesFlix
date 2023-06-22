3305a252bbc702eba130531df6889d490a902691
Victor Paranhos <victor_paranhos@hotmail.com>
2023-06-22 12:05:18 -0300

    add

--- index.js
+++ index.js
@@ -25,19 +25,49 @@ class DadosMovies {
     searchMovies(dados){
 
         this.removeHTML()  
-        Array.from(dados).forEach(dado => {
+        Array.from(dados).forEach(async dado => {
 
-            function VerificarIdioma(valor){
+            const options = {
+                method: 'GET',
+                headers: {
+                  accept: 'application/json',
+                  Authorization: `${apiKey.bearer}`
+                }
+              };
 
-                switch (valor) {
-                    case 'en':
-                      return "Ingles";
-                    case 'pt':
-                      return "Portugues";
-                    default:
-                      return "Idioma não Identificado";
-                  }
+              async function IdentificadorDeLingua(valor1){
+                const Lingua = await fetch('https://api.themoviedb.org/3/configuration/languages', options)
+                .then(response => response.json())
+                .then(response => response)
+                .catch(err => console.error(err));
+
+                const elemento = Array.from(Lingua).find(item => item.iso_639_1 == valor1);
+                
+                return elemento.name
+    
+            
             }
+
+    
+            const lingua = await IdentificadorDeLingua(dado.original_language)               
+
+            async function IdentificadorDeGenero(valor1){
+                const genre = await fetch('https://api.themoviedb.org/3/genre/movie/list?language=pt', options)
+                .then(response => response.json())
+                .then(response => response.genres)
+                .catch(err => console.error(err));
+                
+                const resultado = Array.from(valor1).map(id => {
+                    const elemento = Array.from(genre).find(item => item.id == id);
+                    return elemento.name
+                })
+
+                return resultado
+            
+            }
+
+           const genero = await IdentificadorDeGenero(dado.genre_ids)   
+    
             function converterData(valor){
                 let data = valor.split("-")
                 let [ano, mes, dia] = data
@@ -47,12 +77,12 @@ class DadosMovies {
             }
           
             let row = this.createHTML()
-            IdentificadorDeGenero(dado.genre_ids)
+            
             row.querySelector(".card:has(img) img").src = `https://image.tmdb.org/t/p/w300${dado.poster_path}`
             row.querySelector(".card:has(img) img").alt = `Imagem do filme ${dado.title}`
             row.querySelector(".movie-title").textContent = dado.title
-            row.querySelector(".movie-type").textContent = `AMA`
-            row.querySelector(".movie-language").textContent = `${VerificarIdioma(dado.original_language)}`
+            row.querySelector(".movie-type").textContent = `${genero ?? 'Desconhecido'}`
+            row.querySelector(".movie-language").textContent = `${lingua ?? 'Desconhecido'}`
             row.querySelector(".movie-release").textContent = `Data de Lançamento: ${converterData(dado.release_date)}`
             row.querySelector(".movie-description").textContent = dado.overview
 
@@ -65,11 +95,10 @@ class DadosMovies {
     }
 
     async GetMovies(query){
-        const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(query)}&language=pt-BR`;
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey.key}&query=${encodeURIComponent(query)}&language=pt-BR`;
         const dados = await fetch(url).then(res => res.json()).then(data => data.results)
         this.searchMovies(dados)   
         
-      
     }
 
 
@@ -79,52 +108,12 @@ class DadosMovies {
 const buttonSearch = document.querySelector("#button-search")
 const boxSearch = document.querySelector("#query")
 
-
 buttonSearch.addEventListener("click", (event)=> {
     event.preventDefault();
     const {value} = boxSearch
-    
     const Dados = new DadosMovies();
     Dados.GetMovies(value)
 
-  
-
-
 })
-
-const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNGYwMDY5ZGRhMDg0OWI3ZDNiODg5MTg5NTY2ZWQ4MiIsInN1YiI6IjY0OTFmZjVjYzNjODkxMDBhZTUyYmQ2OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.V0rnkuKqr86IOKVuI8g8ee5OMOdKVw-R_Izg7npBiRk'
-    }
-  };
-
-async function IdentificadorDeGenero(gen){
-    const genre = await fetch('https://api.themoviedb.org/3/genre/movie/list?language=pt', options)
-    .then(response => response.json())
-    .then(response => {
-       converteIDGenero(gen, response.genres)
-    })
-    .catch(err => console.error(err));
-
-}
-
-function converteIDGenero(valor1, valor2){
-    const resultado = Array.from(valor1).map(id => {
-        const elemento = Array.from(valor2).find(item => item.id === id);
-        return elemento.name
-    })
-
-    
-}   
-
-
-
-  
-
-
-
-
 
 new DadosMovies()
