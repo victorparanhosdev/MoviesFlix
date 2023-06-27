dd648d9a3b29c01e2e9837bcadb5f1f86d46a3c6
Victor Paranhos <victor_paranhos@hotmail.com>
2023-06-27 17:08:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -25,70 +25,30 @@ class DadosMovies {
     searchMovies(dados){
 
         this.removeHTML()  
-        Array.from(dados).forEach(async dado => {
 
-            const options = {
-                method: 'GET',
-                headers: {
-                  accept: 'application/json',
-                  Authorization: `${apiKey.bearer}`
-                }
-              };
 
-              async function IdentificadorDeLingua(valor1){
-                const Lingua = await fetch('https://api.themoviedb.org/3/configuration/languages', options)
-                .then(response => response.json())
-                .then(response => response)
-                .catch(err => console.error(err));
+        function converterData(valor){
+            let data = valor.split("-")
+            let [ano, mes, dia] = data
+            let dataconvertida = `${dia}/${mes}/${ano}`
+            return dataconvertida
 
-                const elemento = Array.from(Lingua).find(item => item.iso_639_1 == valor1);
-                
-                return elemento.name
-    
-            
-            }
+        }
 
-    
-            const lingua = await IdentificadorDeLingua(dado.original_language)               
-
-            async function IdentificadorDeGenero(valor1){
-                const genre = await fetch('https://api.themoviedb.org/3/genre/movie/list?language=pt', options)
-                .then(response => response.json())
-                .then(response => response.genres)
-                .catch(err => console.error(err));
-                
-                const resultado = Array.from(valor1).map(id => {
-                    const elemento = Array.from(genre).find(item => item.id == id);
-                    return elemento.name
-                })
-
-                return resultado
-            
-            }
-
-           const genero = await IdentificadorDeGenero(dado.genre_ids)   
-    
-            function converterData(valor){
-                let data = valor.split("-")
-                let [ano, mes, dia] = data
-                let dataconvertida = `${dia}/${mes}/${ano}`
-                return dataconvertida
-
-            }
+   
           
             let row = this.createHTML()
             
-            row.querySelector(".card:has(img) img").src = `https://image.tmdb.org/t/p/w300${dado.poster_path}`
-            row.querySelector(".card:has(img) img").alt = `Imagem do filme ${dado.title}`
-            row.querySelector(".movie-title").textContent = dado.title
-            row.querySelector(".movie-type").textContent = `${genero ?? 'Desconhecido'}`
-            row.querySelector(".movie-language").textContent = `${lingua ?? 'Desconhecido'}`
-            row.querySelector(".movie-release").textContent = `Data de Lançamento: ${converterData(dado.release_date)}`
-            row.querySelector(".movie-description").textContent = dado.overview
-
+            row.querySelector(".card:has(img) img").src = `https://image.tmdb.org/t/p/w200${dados.poster_path}`
+            row.querySelector(".card:has(img) img").alt = `Imagem do filme ${dados.title}`
+            row.querySelector(".movie-title").textContent = dados.title
+            row.querySelector(".movie-type").textContent = `Genero`
+            row.querySelector(".movie-language").textContent = `Lingua`
+            row.querySelector(".movie-release").textContent = `Data de Lançamento: ${converterData(dados.release_date)}`
+            row.querySelector(".movie-description").textContent = dados.overview
             document.querySelector("#movies").append(row)
             
-        })
+        
       
       
         
@@ -97,8 +57,23 @@ class DadosMovies {
     async GetMovies(query){
         const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey.key}&query=${encodeURIComponent(query)}&language=pt-BR`;
         const dados = await fetch(url).then(res => res.json()).then(data => data.results)
-        this.searchMovies(dados)   
+
+      
+        this.GetID(dados)
+    }
+
+    async GetID(id){
+
+        Array.from(id).forEach(async extrairID => {
         
+            const url = `https://api.themoviedb.org/3/movie/${extrairID.id}?api_key=${apiKey.key}`
+            const dados = await fetch(url).then(res => res.json()).then(data => data)
+
+            this.searchMovies(dados)
+        })
+       
+       
+     
     }
 
 
@@ -113,6 +88,7 @@ buttonSearch.addEventListener("click", (event)=> {
     const {value} = boxSearch
     const Dados = new DadosMovies();
     Dados.GetMovies(value)
+
 
 })
 
