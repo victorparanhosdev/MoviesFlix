6f8200f78965ea618c3969cb3aeb04220bae95c7
Victor Paranhos <victor_paranhos@hotmail.com>
2023-07-03 21:30:28 -0300

    Delete apiKey.js

--- apiKey.js
+++
@@ -1,4 +0,0 @@
-export const apiKey = {
-    key:'f4f0069dda0849b7d3b889189566ed82',
-    bearer: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNGYwMDY5ZGRhMDg0OWI3ZDNiODg5MTg5NTY2ZWQ4MiIsInN1YiI6IjY0OTFmZjVjYzNjODkxMDBhZTUyYmQ2OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.V0rnkuKqr86IOKVuI8g8ee5OMOdKVw-R_Izg7npBiRk"
-} 
