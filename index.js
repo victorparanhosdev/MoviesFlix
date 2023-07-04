59274fa400de952f6efe7ca879cec0b9d07703db
Victor Paranhos <victor_paranhos@hotmail.com>
2023-07-03 22:43:58 -0300

    add func

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import {apiKey} from './apiKey.js'
+import { apiKey } from './apiKey.js'
 
 
 class DadosMovies {
-  
-    createHTML(){
+
+    createHTML() {
         const div = document.createElement('div')
         div.classList.add("card")
         div.innerHTML = ` <img src="imagem_do_filme.jpg" alt="Imagem do Filme">
@@ -13,21 +13,21 @@ class DadosMovies {
           <p class="movie-description">Descrição do filme Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         </div>`
 
-       return div
-    
+        return div
+
     }
-    removeHTML(){
+    removeHTML() {
         document.querySelectorAll("#movies .card").forEach(card => card.remove())
     }
 
-  
-
-    searchMovies(dados){
-
-        this.removeHTML()  
 
 
-        function converterData(valor){
+    searchMovies(dados) {
+
+        this.removeHTML()
+
+
+        function converterData(valor) {
             let data = valor.split("-")
             let [ano, mes, dia] = data
             let dataconvertida = `${dia}/${mes}/${ano}`
@@ -37,34 +37,34 @@ class DadosMovies {
 
 
         Array.from(dados).forEach(async extrairID => {
-        
+
             const url = `https://api.themoviedb.org/3/movie/${extrairID.id}?api_key=${apiKey.key}&language=pt-BR`
             const dado = await fetch(url).then(res => res.json()).then(data => data)
-      
+
             let row = this.createHTML()
 
-            async function GeneneroSelected(genero){
+            async function GeneneroSelected(genero) {
 
-               const lista =  Array.from(genero).map(Ongenero =>{
-                        return' ' + Ongenero.name 
+                const lista = Array.from(genero).map(Ongenero => {
+                    return ' ' + Ongenero.name
                 })
 
                 return lista
 
             }
 
-            async function LinguaSelected(linguas){
+            async function LinguaSelected(linguas) {
 
-                const lista =  Array.from(linguas).map(lingua =>{
-                         return' ' + lingua.english_name
-                 })
- 
-                 return lista
- 
-             }
-      
-     
-             
+                const lista = Array.from(linguas).map(lingua => {
+                    return ' ' + lingua.english_name
+                })
+
+                return lista
+
+            }
+
+
+
 
             row.querySelector(".card:has(img) img").src = `https://image.tmdb.org/t/p/w200${dado.poster_path}`
             row.querySelector(".card:has(img) img").alt = `Imagem do filme ${dado.title}`
@@ -75,25 +75,25 @@ class DadosMovies {
             row.querySelector(".movie-description").textContent = dado.overview
             document.querySelector("#movies").append(row)
 
-            
+
         })
-          
-          
-            
-        
-      
-      
-        
+
+
+
+
+
+
+
     }
 
-    async GetMovies(query){
+    async GetMovies(query) {
         const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey.key}&query=${encodeURIComponent(query)}&language=pt-BR`;
         const dados = await fetch(url).then(res => res.json()).then(data => data.results)
 
-      
+
         this.searchMovies(dados)
     }
- 
+
 
 
 }
@@ -101,13 +101,20 @@ class DadosMovies {
 const buttonSearch = document.querySelector("#button-search")
 const boxSearch = document.querySelector("#query")
 
-buttonSearch.addEventListener("click", (event)=> {
+buttonSearch.addEventListener("click", (event) => {
     event.preventDefault();
-    const {value} = boxSearch
+    const { value } = boxSearch
     const Dados = new DadosMovies();
     Dados.GetMovies(value)
+})
 
+const legend = document.querySelector(".movie-description")
 
+legend.addEventListener("click", (event)=> {
+    console.log(event)
+    //event.currentTarget.classList.toggle("expand")
+    //legend.classList.toggle("expand")
 })
 
 new DadosMovies()
+
